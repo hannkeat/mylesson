5e4942ac09195647a5e10950e26e8a42f0908cd5
hannkeat <43680921+hannkeat@users.noreply.github.com>
2021-03-23 23:15:48 +0800

    input

--- FindPercentage.py
+++ FindPercentage.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
-
         student_marks[name] = scores
 
     query_name = input()
