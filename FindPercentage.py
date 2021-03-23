4141d2023cf0f721c2e932a392532f836b4f8bcb
hannkeat <43680921+hannkeat@users.noreply.github.com>
2021-03-23 23:14:49 +0800

    input

---
+++ FindPercentage.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+
+        student_marks[name] = scores
+
+    query_name = input()
+    print("{0:.2f}".format(sum(student_marks[query_name]) / len(scores)))
+
+"""
+sample input
+3
+Krishna 67 68 69
+Arjun 70 98 63
+Malika 52 56 60
+Malika
+
+sample output
+56.00
+"""
