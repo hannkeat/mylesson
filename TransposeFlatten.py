77a7940fd837460ce8c352038803965275fd8daf
hannkeat <43680921+hannkeat@users.noreply.github.com>
2021-03-26 14:55:26 +0800

    initial - cleaner

--- TransposeFlatten.py
+++ TransposeFlatten.py
@@ -5,3 +5,19 @@ array = np.array([input().strip().split() for _ in range(n)], int)
 
 print(np.transpose(array))
 print(array.flatten())
+
+
+"""
+sample input
+
+3 2
+1 2
+3 4
+5 6
+
+expected output
+[[1 3 5]
+ [2 4 6]]
+[1 2 3 4 5 6]
+
+"""
